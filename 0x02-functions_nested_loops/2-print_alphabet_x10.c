cca197c0285a609f87a1ffdce4db11cc242134c6
Faithkiv <faithkiv@gmail.com>
2022-10-25 19:53:50 +0000

    Nawee

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-*print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase
+*print_alphabet_x10 - a function that prints 10 times the alphabet
 */
 void print_alphabet_x10(void)
 {
-int count = 0;	
+int count = 0;
 char letter;
-while (count++ <=9)
+while (count++ <= 9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
+}
 }
