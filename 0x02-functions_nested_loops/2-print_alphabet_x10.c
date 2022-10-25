d08103b8f5128db5c5e8cb1db70ad2e057a7c4c9
Faithkiv <faithkiv@gmail.com>
2022-10-25 19:38:04 +0000

    Nawee

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,11 @@
 */
 void print_alphabet_x10(void)
 {
-int letter, co;
-co = 0;
-while (co < 10)
+int count = 0;	
+char letter;
+while (count++ <=9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
-{
 _putchar(letter);
-}
-co++;
 _putchar('\n');
 }
